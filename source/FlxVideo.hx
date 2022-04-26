d82bbd69f6059a175683040a3b1afdfc3efd9d56
jigsaw-4277821 <mihaialexandru427@yahoo.com>
2022-04-26 14:40:39 +0300

    do a videos test

--- source/FlxVideo.hx
+++ source/FlxVideo.hx
@@ -3,12 +3,8 @@ import openfl.net.NetConnection;
 import openfl.net.NetStream;
 import openfl.events.NetStatusEvent;
 import openfl.media.Video;
-#elseif android
-import extension.videoview.VideoView;
-import android.AndroidTools;
 #else
-import openfl.events.Event;
-import vlc.VlcBitmap;
+import vlc.MP4Handler;
 #end
 import flixel.FlxBasic;
 import flixel.FlxG;
@@ -18,7 +14,7 @@ class FlxVideo extends FlxBasic {
 	public var finishCallback:Void->Void = null;
 	
 	#if desktop
-	public static var vlcBitmap:VlcBitmap;
+	public static var video:MP4Handler;
 	#end
 
 	public function new(name:String) {
@@ -49,97 +45,18 @@ class FlxVideo extends FlxBasic {
 		});
 		netStream.play(name);
 
-	        #elseif android
+		#elseif desktop
 
-                VideoView.playVideo(AndroidTools.getFileUrl(name));
-                VideoView.onCompletion = function(){
-		        if (finishCallback != null){
+		video = new MP4Handler();
+		video.finishCallback = function()
+		{
+				if (finishCallback != null){
 			        finishCallback();
 		        }
-                }             
+		}
+		video.playVideo(name);
 
-		#elseif desktop
-		// by Polybius, check out PolyEngine! https://github.com/polybiusproxy/PolyEngine
-
-		vlcBitmap = new VlcBitmap();
-		vlcBitmap.set_height(FlxG.stage.stageHeight);
-		vlcBitmap.set_width(FlxG.stage.stageHeight * (16 / 9));
-
-		vlcBitmap.onComplete = onVLCComplete;
-		vlcBitmap.onError = onVLCError;
-
-		FlxG.stage.addEventListener(Event.ENTER_FRAME, fixVolume);
-		vlcBitmap.repeat = 0;
-		vlcBitmap.inWindow = false;
-		vlcBitmap.fullscreen = false;
-		fixVolume(null);
-
-		FlxG.addChildBelowMouse(vlcBitmap);
-		vlcBitmap.play(checkFile(name));
 		#end
 	}
-
-	#if desktop
-	function checkFile(fileName:String):String
-	{
-		var pDir = "";
-		var appDir = "file:///" + Sys.getCwd() + "/";
-
-		if (fileName.indexOf(":") == -1) // Not a path
-			pDir = appDir;
-		else if (fileName.indexOf("file://") == -1 || fileName.indexOf("http") == -1) // C:, D: etc? ..missing "file:///" ?
-			pDir = "file:///";
-
-		return pDir + fileName;
-	}
-	
-	public static function onFocus() {
-		if(vlcBitmap != null) {
-			vlcBitmap.resume();
-		}
-	}
-	
-	public static function onFocusLost() {
-		if(vlcBitmap != null) {
-			vlcBitmap.pause();
-		}
-	}
-
-	function fixVolume(e:Event)
-	{
-		// shitty volume fix
-		vlcBitmap.volume = 0;
-		if(!FlxG.sound.muted && FlxG.sound.volume > 0.01) { //Kind of fixes the volume being too low when you decrease it
-			vlcBitmap.volume = FlxG.sound.volume * 0.5 + 0.5;
-		}
-	}
-
-	public function onVLCComplete()
-	{
-		vlcBitmap.stop();
-
-		// Clean player, just in case!
-		vlcBitmap.dispose();
-
-		if (FlxG.game.contains(vlcBitmap))
-		{
-			FlxG.game.removeChild(vlcBitmap);
-		}
-
-		if (finishCallback != null)
-		{
-			finishCallback();
-		}
-	}
-
-	
-	function onVLCError()
-		{
-			trace("An error has occured while trying to load the video.\nPlease, check if the file you're loading exists.");
-			if (finishCallback != null) {
-				finishCallback();
-			}
-		}
-	#end
 	#end
 }
